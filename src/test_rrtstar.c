087fd594173c7c3893b36cb7b58d8c2ccfcea73f
Husky <husky@ttic.edu>
2017-03-20 18:52:00 -0500

    test script for rrtstar modified

--- src/test_rrtstar.c
+++ src/test_rrtstar.c
@@ -7,18 +7,18 @@
 
 #include <lcmtypes/bot_core.h>
 #include <lcmtypes/hr_lcmtypes.h>
-     
+
 static void
 send_obs_message(lcm_t * lcm)
 {
     erlcm_rect_list_t my_sim_obs;
-    my_sim_obs.utime = 0; 
+    my_sim_obs.utime = 0;
 
     my_sim_obs.xy[0] = 0;
     my_sim_obs.xy[1] = 0;
 
     my_sim_obs.num_rects = 2;
-    
+
     erlcm_rect_t *rects = (erlcm_rect_t*)calloc(2, sizeof(erlcm_rect_t));
     rects[0].size[0] = 3;
     rects[0].size[1] = 3;
@@ -34,24 +34,30 @@ send_obs_message(lcm_t * lcm)
     rects[2].dxy[1] = 5;
     */
     my_sim_obs.rects = rects;
-    
+
     erlcm_rect_list_t_publish(lcm, "SIM_RECTS", &my_sim_obs);
     free(rects);
     return;
 }
-     
+
 static void
 send_goal_message(lcm_t * lcm)
 {
-  
-    erlcm_goal_list_t my_goals;
-    my_goals.num_goals = 1;
 
-    erlcm_goal_t *goal  = (erlcm_goal_t*)calloc(1, sizeof(erlcm_goal_t));
-    goal[0].pos[0] = -8; 
+    erlcm_goal_list_t my_goals;
+    my_goals.num_goals = 2;
+
+    erlcm_goal_t *goal  = (erlcm_goal_t*)calloc(2, sizeof(erlcm_goal_t));
+    goal[0].pos[0] = -8;
     goal[0].pos[1] = 3;
     goal[0].size[0] = 3;
     goal[0].size[1] = 3;
+
+    goal[1].pos[0] = -12;
+    goal[1].pos[1] = 6;
+    goal[1].size[0] = 3;
+    goal[1].size[1] = 3;
+
     my_goals.goals = goal;
     erlcm_goal_list_t_publish(lcm, "GOALS", &my_goals);
     free(goal);
@@ -71,7 +77,7 @@ main(int argc, char ** argv)
 
     //lcm_destroy(lcm);
 
-    send_obs_message(lcm);
+    //send_obs_message(lcm);
 
     lcm_destroy(lcm);
     return 0;
